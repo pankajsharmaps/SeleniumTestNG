58f96709b88e56144baf8d11ece116d6ccd23de7
Pankaj <1808PankajSharma@gmail.com>
2024-04-27 13:33:14 +0530

    Some test addition

---
+++ src/test/java/testCase/KayakSmokeTest.java
@@ -0,0 +1,13 @@
+package testCase;
+
+import org.testng.annotations.Test;
+
+public class KayakSmokeTest {
+	
+	@Test
+	public void loginCheckTest() {
+		
+		System.out.println("Some test");
+	}
+
+}
